4c89fc740f1bc562a75e12b5a112914d82a797fb
smokytheangel0 <j.m.a.kierstead@gmail.com>
2018-10-24 15:41:05 -0700

    DOES NOT BUILD switching to laptop

--- lib/main.dart
+++ lib/main.dart
@@ -11,116 +11,121 @@ import 'patron/patron_view.dart' as patron;
 import 'spacing/student/student_view.dart' as student;
 //this is the view for site leaders and NGO architect
 import 'spacing/chief/chief_view.dart' as chief;
+
+//this is the left tab view
+import 'login_view.dart' as login;
+//this is the right tab view
+import 'signup_view.dart' as signup;
+
 //this is the root file for functionality
 import 'common/functions.dart' as functions;
 //this is the file for objects (including widgets)
 import 'common/objects.dart' as objects;
 
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MaterialApp(
+    home: new PageFrame0(),
+    routes: <String, WidgetBuilder> {
+      '/LeaderView': (BuildContext context) => new leader.LeaderView(),
+      '/MatronView': (BuildContext context) => new matron.MatronView(),
+      '/NationView': (BuildContext context) => new nation.NationView(),
+      '/PatronView': (BuildContext context) => new patron.PatronView(),
+      '/StudentView': (BuildContext context) => new student.StudentView(),
+      '/ChiefView': (BuildContext context) => new chief.ChiefView(),
+    }
+  ));
+}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class PageFrame0 extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  PageFrame0State createState() => new PageFrame0State();
+}
+
+
+class PageFrame0State extends State<PageFrame0>
+with SingleTickerProviderStateMixin {
+
+  TabController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = new TabController(vsync: this, length: 3);
+
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<bool> _onWillPop() {
+    if (controller.index == 0) {
+      return new Future<bool>.value(true);
+    } else {
+      controller.index = 0;
+      return new Future<bool>.value(false);
+    }
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  Widget build(BuildContext context){
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget> [
+              SliverOverlapAbsorber(
+                handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                child: SliverAppBar(
+                  backgroundColor: const Color(0xff000000),
+                  title: Text("*atron, come on in"),
+                  pinned: true,
+                  expandedHeight: 256.0,
+                  forceElevated: innerBoxIsScrolled,
+                  flexibleSpace: FlexibleSpaceBar(
+                    background: Stack(
+                      fit: StackFit.expand,
+                      children: <Widget> [
+                        Image.asset(
+                          'assets/trees_and_me.jpg',
+                          fit: BoxFit.cover,
+                          height: 256.0
+                        )
+                      ]
+                    )
+                  ),
+
+                  bottom: TabBar(
+                    controller: controller,
+                    tabs: <Tab>[
+                      Tab(
+                        icon: ImageIcon(
+                          AssetImage("assets/loginIcon.png")
+                        )
+                      ),
+                      Tab(
+                        icon:ImageIcon(
+                          AssetImage("assets/signupIcon.png")
+                        )
+                      )
+                    ],
+                  )
+                )
+              )
+            ];
+          },
+          body: new TabBarView(
+            controller: controller,
+            children: <Widget>[
+              new login.LoginView(),
+              new signup.SignupView(),
+            ]
+          )
+        )
+      )
     );
   }
 }
